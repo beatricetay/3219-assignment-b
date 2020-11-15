7efaa2dc74a457e8f7ddb417354341c8211f3a85
beatricetay <beatricetayyt@gmail.com>
2020-11-15 20:06:19 +0800

    task b3: edited credentials file name in serverless.yml

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,2 +1,3 @@
-printf '%s\n' "$KEYFILE" > "$TRAVIS_BUILD_DIR/google_credentials"
+printf '%s\n' "$google_credentials" > "$TRAVIS_BUILD_DIR/google_credentials.json"
+echo $TRAVIS_BUILD_DIR
 serverless deploy
